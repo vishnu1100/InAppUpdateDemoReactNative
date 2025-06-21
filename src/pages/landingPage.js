91443e4cd5892d31be04312d95ac2e3aa62f981b
vishnu1100 <vishnusanthosh.india@gmail.com>
2025-06-21 16:35:29 +0530

    add landingPage component with basic structure

---
+++ src/pages/landingPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class landingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> landingPage </Text>
+      </View>
+    );
+  }
+}
+
+export default landingPage;
